6ee71ff4b3d9a2fdcf52b00f64bc08f86976d657
Jordan Murphy <jmurphy6895@gmail.com>
2025-01-28 14:26:53 -0700

    chore: fix tests

--- test/differentiability/test_dynamics_builder.jl
+++ test/differentiability/test_dynamics_builder.jl
@@ -42,43 +42,22 @@ end
 
 @testset "Dynamics Builder Parameter Differentiability" begin
     SpaceIndices.init()
-    eop_data = fetch_iers_eop()
-    grav_coeffs = GravityModels.load(IcgemFile, fetch_icgem_file(:EGM96))
-
-    JD = date_to_jd(2024, 1, 5, 12, 32, 0.0)
-    p = ComponentVector(; JD=JD)
 
     function dynamics_params(x::AbstractArray)
-        grav_model = GravityHarmonicsAstroModel(;
-            gravity_model=grav_coeffs, eop_data=eop_data, order=36, degree=36
-        )
-        sun_third_body = ThirdBodyModel(; body=SunBody(), eop_data=eop_data)
-        moon_third_body = ThirdBodyModel(; body=MoonBody(), eop_data=eop_data)
-
         satellite_srp_model = CannonballFixedSRP(x[1])
         srp_model = SRPAstroModel(;
             satellite_srp_model=satellite_srp_model,
-            sun_data=sun_third_body,
-            eop_data=eop_data,
+            sun_data=_sun_model,
+            eop_data=_eop_data,
             shadow_model=Conical(),
         )
 
         satellite_drag_model = CannonballFixedDrag(x[2])
-        drag_model = DragAstroModel(;satellite_drag_model=satellite_drag_model, atmosphere_model=JB2008(), eop_data=eop_data)
+        drag_model = DragAstroModel(;satellite_drag_model=satellite_drag_model, atmosphere_model=JB2008(), eop_data=_eop_data)
 
-        state = [
-            -1076.225324679696
-            -6765.896364327722
-            -332.3087833503755
-            9.356857417032581
-            -3.3123476319597557
-            -1.1880157328553503
-        ] #km, km/s
+        model_list = (_grav_model, _sun_model, _moon_model, srp_model, drag_model)
 
-        t = 0.0
-        model_list = (grav_model, sun_third_body, moon_third_body, srp_model, drag_model)
-
-        return Array(build_dynamics_model(state, p, t, model_list))
+        return Array(build_dynamics_model(_state, _p, _t, model_list))
     end
 
     p_spacecraft = [0.2, 0.2]
@@ -96,7 +75,7 @@ end
             f_ad, df_ad = value_and_jacobian(dynamics_params, backend[2], p_spacecraft)
 
             @test f_fd ≈ f_ad
-            @test df_fd ≈ df_ad rtol = 1e-2
+            @test df_fd ≈ df_ad atol = 1e-9
         end
     end
     SpaceIndices.destroy()
