2ff8648619067146c45ce2a843e71b9ff761afc0
Jordan Murphy <jmurphy6895@gmail.com>
2025-11-21 13:44:50 -0700

    chore: remove unused CI's and uncomment tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -43,21 +43,21 @@ using JET
 end
 
 _BACKENDS = (
-#    ("ForwardDiff", AutoForwardDiff()),
-#    ("Enzyme", AutoEnzyme(; mode=Enzyme.set_runtime_activity(Enzyme.Forward))),
+    ("ForwardDiff", AutoForwardDiff()),
+    ("Enzyme", AutoEnzyme(; mode=Enzyme.set_runtime_activity(Enzyme.Forward))),
     ("Mooncake", AutoMooncake(; config=nothing)),
-#    ("PolyesterForwardDiff", AutoPolyesterForwardDiff()),
-#    ("Zygote", AutoZygote()),
+    ("PolyesterForwardDiff", AutoPolyesterForwardDiff()),
+    ("Zygote", AutoZygote()),
 )
 
 @testset "Differentiability" begin
     include("differentiability/test_model_parameters.jl")
     include("differentiability/test_drag.jl")
-    #include("differentiability/test_srp.jl")
-    #include("differentiability/test_gravity.jl")
-    #include("differentiability/test_relativity.jl")
-    #include("differentiability/test_third_body.jl")
-    #include("differentiability/test_dynamics_builder.jl")
+    include("differentiability/test_srp.jl")
+    include("differentiability/test_gravity.jl")
+    include("differentiability/test_relativity.jl")
+    include("differentiability/test_third_body.jl")
+    include("differentiability/test_dynamics_builder.jl")
 end
 
 @testset "Performance" begin
